693727e9016b23029dd9be9403e5059b3d8e273a
Riyan Agarwal <agarwalriyan200116@gmail.com>
2023-03-31 03:02:25 +0000

    Fix indentations

--- QueriesAndSP.sql
+++ QueriesAndSP.sql
@@ -1,39 +1,30 @@
-SELECT 
-	FirstName+ ' ' + LastName AS Name,
+SELECT FirstName+ ' ' + LastName AS Name,
 	DATEDIFF(DAY,Dob,CAST(GETDATE() AS Date)) AS [Age in days]
-FROM 
-	Foundation.Actors
+FROM Foundation.Actors
 
 
-SELECT  
-	DISTINCT A.Id,
+SELECT  DISTINCT A.Id,
 	A.FirstName,
 	A.LastName
-FROM 
-	Foundation.Producers P
+FROM Foundation.Producers P
 	INNER JOIN Foundation.Movies M
 		ON P.Id = M.ProducerId
 	INNER JOIN Foundation.Actors_Movies AM
 		ON M.Id = AM.MovieId
 	INNER JOIN Foundation.Actors A
 		ON A.Id = AM.ActorId
-WHERE
-	P.Id = 2
+WHERE P.Id = 2
 
-SELECT
-	AM1.ActorId AS [First actor],
+
+SELECT AM1.ActorId AS [First actor],
 	AM2.ActorId AS [Second actor]
-FROM 
-	Foundation.Actors_Movies AM1
+FROM Foundation.Actors_Movies AM1
 	CROSS JOIN Foundation.Actors_Movies AM2
-WHERE 
-	AM1.MovieId = AM2.MovieId 
+WHERE AM1.MovieId = AM2.MovieId 
 	AND AM1.ActorId < AM2.ActorId
-Group by 
-	Am1.ActorId,
+Group by Am1.ActorId,
 	AM2.ActorId
-HAVING 
-	COUNT(*)>=2
+HAVING COUNT(*)>=2
 
 
 SELECT TOP 1 
@@ -43,63 +34,43 @@ SELECT TOP 1
 	Bio,
 	Sex,
 	Dob AS [Date of birth]
-FROM 
-	Foundation.Actors
-ORDER BY 
-	Dob DESC
+FROM Foundation.Actors
+ORDER BY Dob DESC
 
 
-SELECT 
-	AM1.ActorId AS [First actor],
+SELECT AM1.ActorId AS [First actor],
 	AM2.ActorId AS [Second actor]
-FROM
-	Foundation.Actors_Movies AM1
+FROM Foundation.Actors_Movies AM1
 	CROSS JOIN Foundation.Actors_Movies AM2
-WHERE
-	AM1.ActorId < AM2.ActorId
+WHERE AM1.ActorId < AM2.ActorId
 EXCEPT
-SELECT 
-	AM1.ActorId,
+SELECT AM1.ActorId,
 	AM2.ActorId
-FROM
-	Foundation.Actors_Movies AM1
+FROM Foundation.Actors_Movies AM1
 	CROSS JOIN Foundation.Actors_Movies AM2
-WHERE
-	AM1.MovieId = AM2.MovieId 
+WHERE AM1.MovieId = AM2.MovieId 
 	AND AM1.ActorId < AM2.ActorId
 
 
-
-SELECT
-	Language,
+SELECT Language,
 	COUNT(Id) AS [Number of Movies]
-FROM
-	Foundation.Movies
-GROUP BY
-	Language
+FROM Foundation.Movies
+GROUP BY Language
 
 
-SELECT
-	Language,
+SELECT Language,
 	SUM(Profit) AS [Total profit]
-FROM
-	Foundation.Movies
-GROUP BY
-	Language
+FROM Foundation.Movies
+GROUP BY Language
 
 
-SELECT
-	Language,
+SELECT Language,
 	SUM(Profit) AS [Total profit]
-FROM
-	Foundation.Movies M
+FROM Foundation.Movies M
 	INNER JOIN Foundation.Actors_Movies AM
 		ON M.Id = AM.MovieId
-WHERE
-	AM.ActorId=1
-GROUP BY
-	Language
-
+WHERE AM.ActorId=1
+GROUP BY Language
 
 
 CREATE PROC Foundation.usp_insert_Movie
@@ -116,7 +87,6 @@ AS
 INSERT INTO 
 	Foundation.Movies(Id,Name,Plot,YearOfRelease,Poster,ProducerId,Profit,UpdatedAt,Language) 
 	VALUES(@Id,@Name,@Plot,@Year,@Poster,@ProducerId,@Profit,Null,@Language)
-
 DECLARE @MaxId int
 SELECT @MaxId=MAX(Id)
 FROM Foundation.Actors_Movies
@@ -129,16 +99,13 @@ SELECT @MaxId+ordinal,
 FROM string_split(@ActorList,',',1)
 
 
-
 CREATE PROC Foundation.usp_delete_Movie
 @Id int
 AS
 DELETE FROM Foundation.Actors_Movies
 WHERE MovieId = @Id;
-
 DELETE FROM Foundation.Movies
 WHERE Id = @Id;
-
 
 
 CREATE PROC Foundation.usp_delete_Producer
@@ -148,7 +115,6 @@ DECLARE @RowCount int =
 	(SELECT COUNT(*) 
 	FROM Foundation.Movies
 	WHERE ProducerId=@Id)
-
 DECLARE @MovieToDelete int
 WHILE @RowCount>0
 BEGIN
@@ -168,6 +134,5 @@ CREATE PROC Foundation.usp_delete_Actor
 AS
 DELETE FROM Foundation.Actors_Movies
 WHERE @Id=ActorId
-
 DELETE FROM Foundation.Actors
 WHERE @Id=Id
